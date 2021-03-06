f85f62254cd173cf96abdbda83896d5f4a104777
ysma500 <ysmael.fortier@gmail.com>
2021-03-05 22:11:46 -0500

    updated setup script

--- scripts/git_setup.sh
+++ scripts/git_setup.sh
@@ -3,7 +3,7 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0; \
 sudo apt-add-repository https://cli.github.com/packages; \
 sudo apt update; \
 sudo apt install gh -y; \
-gh auth login --hostname github.com; \
+gh auth status; \
 echo "Add ssh key in ~/.ssh/id_rsa file here"; \
 curl -s https://api.github.com/orgs/ubivius/repos | \
 grep "\"name\": \"telemetry" | \
