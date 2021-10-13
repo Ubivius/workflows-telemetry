b918e6055b5eb67d2b58f4974a4b28eb7a06bd8b
ysma500 <ysmael.fortier@gmail.com>
2021-10-12 22:03:09 -0400

    Changes include lke cd add, changed telemetry projects and more doc describing the process

--- src/.github/tools_install.sh
+++ src/.github/tools_install.sh
@@ -3,9 +3,10 @@ sudo echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 sudo apt-get update
 sudo apt-get install google-cloud-sdk -y
+brew install --build-from-source yq jq
+mkdir ~/.kube
 helm repo add elastic https://helm.elastic.co
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo add kube-state-metrics https://kubernetes.github.io/kube-state-metrics
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
 helm repo update
